3c842ed83e4737e2e790d6291f1128a098b3ed13
Nitin Gulwani <74307374+nitingulwani@users.noreply.github.com>
2023-06-27 01:14:27 +0530

    Create reverseArray.java

---
+++ reverseArray.java
@@ -0,0 +1,23 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static void reverseArray(ArrayList<Integer> arr, int m)
+    {
+        // Write your code here.
+        int n = arr.size();
+        int i = m+1;
+        int j = n-1;
+
+        while(i<j){
+            int temp = arr.get(i);
+            arr.set(i, arr.get(j));
+            arr.set(j, temp);
+            i++;
+            j--;
+        }
+        return;
+    }
+}
