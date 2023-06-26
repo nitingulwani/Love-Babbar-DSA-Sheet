c3d84c9b0fc05cae9af11a97fc7b5e0adc497bb9
Nitin Gulwani <74307374+nitingulwani@users.noreply.github.com>
2023-06-27 01:16:02 +0530

    Update minMaxSum.java

--- maxMinSum/minMaxSum.java
+++ maxMinSum/minMaxSum.java
@@ -1,1 +1,17 @@
 
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+  public static int sumOfMaxMin(int[] arr, int n) {
+      // Write your code here.
+      int min = Integer.MAX_VALUE;
+      int max = Integer.MIN_VALUE;
+
+      for(int i = 0; i<n; i++){
+        min = Math.min(min, arr[i]);
+        max = Math.max(max, arr[i]);
+      }
+      int sum = min + max;
+      return sum;
+  }
+}
